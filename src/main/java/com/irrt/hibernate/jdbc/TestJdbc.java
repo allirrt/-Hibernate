7775b71b092052c836989f6a441ec9074c2e5e5c
IRRT <zindgg@gmail.com>
2021-02-19 12:08:33 +0300

    One-To-One/ Create table InstructorDetail

--- src/main/java/com/irrt/hibernate/jdbc/TestJdbc.java
+++ src/main/java/com/irrt/hibernate/jdbc/TestJdbc.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 public class TestJdbc {
     public static void main(String[] args) {
 
-        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
-        String user = "hbstudent";
-        String pass = "hbstudent";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+        String user = "one-to-one-uni";
+        String pass = "one-to-one-uni";
 
         try {
             System.out.println("Connecting to database" + jdbcUrl);
